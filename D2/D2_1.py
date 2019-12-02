e686963d518deb13783d2d96e620d43a892b6ada
mineiwik <developer@codingcorals.ch>
2019-12-02 15:35:24 +0100

    Day 2 Part 1

--- D2/D2_1.py
+++ D2/D2_1.py
@@ -1,6 +1,20 @@
 # AoC 2019 - Day 2 - Part 1
 
 f = open('input.txt', 'r')
+codes = list(map(int, f.read().split(',')))
 
-for l in f:
-    print(l)
+codes[1] = 12
+codes[2] = 2
+
+i = 0
+for code in codes:
+    if i % 4 == 0:
+        if code == 1:
+            codes[codes[i + 3]] = codes[codes[i + 1]] + codes[codes[i + 2]]
+        elif code == 2:
+            codes[codes[i + 3]] = codes[codes[i + 1]] * codes[codes[i + 2]]
+        elif code == 99:
+            break
+    i += 1
+
+print(codes[0])
--- D2/D2_2.py
+++ D2/D2_2.py
@@ -1,6 +1,1 @@
 # AoC 2019 - Day 2 - Part 2
-
-f = open('input.txt', 'r')
-
-for l in f:
-    print(l)
